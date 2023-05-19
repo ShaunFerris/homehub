f20df6b8305d31a03b682f088d3d71682baca980
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-19 14:32:00 +0200

    Install dependancies, setup tailwind custom classes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className='text-9xl text-amber-600'>Hello World</div>
+    <div className='text-9xl text-amber-600 text-center align-text-top'>
+        Hello World
+    </div>
   )
 }
