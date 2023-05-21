bb045408884518da98b26b34f485cecb4bb7251e
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-21 12:40:21 +0200

    Start home page layout and navbar component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 const Home = () => {
     return (
-        <div>page</div>
+        <section className="flex-center flex-col w-full">
+            <h1 className="head_text text-center">
+                homeHub
+                <br className="max-md:hidden" />
+                <span className="text-center orange_gradient">
+                    Get Your Shit Together
+                </span>
+            </h1>
+            <p className="desc text-center">
+                Organize your life and keep track of tasks that need
+                to be done to keep your house in order.
+            </p>
+
+            {/* Login Component? */}
+        </section>
     );
 };
 
