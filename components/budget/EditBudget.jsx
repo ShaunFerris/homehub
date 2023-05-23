b14d00ddfd841079622330dc72ad666da0d885c1
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-23 15:27:40 +0200

    Implement and style edit budget

--- components/budget/EditBudget.jsx
+++ components/budget/EditBudget.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import { useState } from "react";
 
-const EditBudget = () => {
+const EditBudget = (props) => {
+    const [value, setValue] = useState(props.budget);
+
     return (
-        <div>EditBudget</div>
+        <>
+            <input
+                className="mr-3 form_input"
+                required="required"
+                type="number"
+                id="budgetAmount"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+            />
+
+            <button
+                className="black_btn"
+                onClick={() => {
+                    return props.handleSaveEdit(value);
+                }}
+            >
+                Save
+            </button>
+        </>
     );
 };
 
