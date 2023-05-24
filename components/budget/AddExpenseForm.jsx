59165a7ad50af718486845a095ec89025f9d8b76
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-24 11:18:39 +0200

    Start add expense form

--- components/budget/AddExpenseForm.jsx
+++ components/budget/AddExpenseForm.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import { useState, useContext } from "react";
+import { BudgetContext } from "@/context/budgetContext";
+import { v4 as uuidv4 } from "uuid";
 
 const AddExpenseForm = () => {
+    const { dispatch } = useContext(BudgetContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInte(cost)
+        };
+    };
+
+    dispatch({
+        type: "ADD_EXPENSE",
+        payload: expense
+    });
+
     return (
-        <div>AddExpenseForm</div>
+        <form onSubmit={onSubmit}>
+            <div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        className="form_input"
+                        required="required"
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    ></input>
+                </div>
+            </div>
+        </form>
     );
 };
 
