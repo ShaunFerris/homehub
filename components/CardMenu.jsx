0bd793876a7b03e1c39a8edcb2b7e7e81a22ea5a
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-25 10:43:27 +0200

    Add static and animated gifs for all the card menu elements

--- components/CardMenu.jsx
+++ components/CardMenu.jsx
@@ -17,46 +17,75 @@ const CardMenu = () => {
         <div className="flex justify-center items-center space-x-4">
             <div className="w-48 h-48 bg-blue-100 shadow-lg rounded-lg
                 p-4">
-                <Link href="/budget">
-                    <h3 className="text-xl font-bold mb-2">
-                        Budget Tracker
-                    </h3>
-                </Link>
                 <div
                     className="relative w-40 h-40"
                     onMouseEnter={handleHover}
                     onMouseLeave={handleUnHover}
                 >
-                    <Image
-                        src={isHovered ? '/assets/images/piggybank_animated.gif' : '/assets/images/piggybank_still.gif'}
-                        alt="Piggybank"
-                        className="w-full h-full"
-                        width={30}
-                        height={30}
-                    />
+                    <Link href="/budget">
+                        <h3 className="text-xl font-bold mb-2">
+                            Budget Tracker
+                        </h3>
+
+                        <Image
+                            src={isHovered ?
+                                '/assets/images/piggybank_animated.gif' :
+                                '/assets/images/piggybank_still.gif'}
+                            alt="piggybank"
+                            className="w-full h-full pb-5"
+                            width={20}
+                            height={20}
+                        />
+                    </Link>
                 </div>
             </div>
             <div className="w-48 h-48 bg-blue-100 shadow-lg rounded-lg
                 p-4">
-                <Link href="/todo">
-                    <h3 className="text-xl font-bold mb-2">
-                        TODOs
-                    </h3>
-                </Link>
-                <p className="text-gray-600 ">
-                    Card content goes here
-                </p>
+                <div
+                    className="relative w-40 h-40"
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleUnHover}
+                >
+                    <Link href="/todo">
+                        <h3 className="text-xl font-bold mb-2">
+                            TODO List
+                        </h3>
+
+                        <Image
+                            src={isHovered ?
+                                '/assets/images/todolist_animated.gif' :
+                                '/assets/images/todolist_still.gif'}
+                            alt="todolist"
+                            className="w-full h-full pb-5"
+                            width={20}
+                            height={20}
+                        />
+                    </Link>
+                </div>
             </div>
             <div className="w-48 h-48 bg-blue-100 shadow-lg rounded-lg
                 p-4">
-                <Link href="/shoplist">
-                    <h3 className="text-xl font-bold mb-2">
-                        Shopping List
-                    </h3>
-                </Link>
-                <p className="text-gray-600 ">
-                    Card content goes here
-                </p>
+                 <div
+                    className="relative w-40 h-40"
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleUnHover}
+                >
+                    <Link href="/shoplist">
+                        <h3 className="text-xl font-bold mb-2">
+                            Shopping List
+                        </h3>
+
+                        <Image
+                            src={isHovered ?
+                                '/assets/images/shopping_animated.gif' :
+                                '/assets/images/shopping_still.gif'}
+                            alt="shoppinglist"
+                            className="w-full h-full pb-5"
+                            width={20}
+                            height={20}
+                        />
+                    </Link>
+                </div>
             </div>
         </div>
     );
