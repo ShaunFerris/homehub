3984df7e23322dfa5f8cacd46a91361c8b7a663d
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-25 09:53:11 +0200

    Start adding animated icons to the card menu

--- components/CardMenu.jsx
+++ components/CardMenu.jsx
@@ -1,6 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 const CardMenu = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(true);
+    };
+
+    const handleUnHover = () => {
+        setIsHovered(false);
+    };
+
     return (
         <div className="flex justify-center items-center space-x-4">
             <div className="w-48 h-48 bg-blue-100 shadow-lg rounded-lg
@@ -10,9 +22,19 @@ const CardMenu = () => {
                         Budget Tracker
                     </h3>
                 </Link>
-                <p className="text-gray-600 ">
-                    Card content goes here
-                </p>
+                <div
+                    className="relative w-40 h-40"
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleUnHover}
+                >
+                    <Image
+                        src={isHovered ? '/assets/images/piggybank_animated.gif' : '/assets/images/piggybank_still.gif'}
+                        alt="Piggybank"
+                        className="w-full h-full"
+                        width={30}
+                        height={30}
+                    />
+                </div>
             </div>
             <div className="w-48 h-48 bg-blue-100 shadow-lg rounded-lg
                 p-4">
