d313087fa9dd759f524284e2995f3429b8128fd1
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-26 12:41:01 +0200

    Add conditional check to the pending tasks list

--- components/todo/PendingList.jsx
+++ components/todo/PendingList.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const PendingList = () => {
+    const pendingTasks = [];
+
     return (
         <div className='card_container_vert'>
             <h1 className='subhead_text text-center'>
                 Pending Tasks
             </h1>
+            {pendingTasks.length === 0 ? (
+                <p className='desc text-center'>
+                    No tasks are pending right now
+                </p>
+            ) : (
+                <div>Pending Tasks</div>
+            )
+
+            }
         </div>
     );
 };
