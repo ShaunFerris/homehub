9d475787118eec3665654d7f75a865c620aa1882
Shaun Ferris <slb.ferris46@gmail.com>
2023-06-05 15:03:20 +0200

    Start templating the shopping list route

--- app/shoplist/page.js
+++ app/shoplist/page.js
@@ -1,8 +1,22 @@
+"use client";
+
 import React from 'react';
 
 const Shoplist = () => {
     return (
-        <div>Shoplist</div>
+        <section className='flex flex-col items-center justify-center
+        w-full'>
+            <h1 className='head_text text-center blue_gradient py-3'>
+                Shopping List
+            </h1>
+            <div className='card_container w-full flex flex-row gap-4'>
+                <input 
+                    className='form_input border border-black'
+                    placeholder='Add an item to your shopping list...'
+                />
+                <button className='black_btn'>Add</button>
+            </div>
+        </section>
     );
 };
 
