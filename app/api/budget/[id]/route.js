d81f42fd2c7711955ab1b25d0d21fe4a6c0ca165
Shaun Ferris <slb.ferris46@gmail.com>
2023-06-22 10:27:22 +0200

    Add dynamic patch endpoint for budget route

--- app/api/budget/[id]/route.js
+++ app/api/budget/[id]/route.js
@@ -6,8 +6,28 @@ export const GET = async (req, { params }) => {
         await connectToDB();
         const data = await Budget.findById(params.id);
         return new Response(JSON.stringify(data), { status: 200 });
-        
+
     } catch (error) {
         return new Response("Failed to fetch budget", { status: 500 });
+    }
+};
+
+export const PATCH = async (req, { params }) => {
+    const { name, budget, expenses } = await req.json();
+
+    try {
+        await connectToDB();
+
+        await Budget.findByIdAndUpdate(
+            params.id,
+            {
+                name: name,
+                budget: budget,
+                expenses: expenses
+            }
+        );
+        return new Response("Budget data updated", { status: 200 });
+    } catch (error) {
+        return new Response("Budget update failed", { status: 500 });
     }
 };
