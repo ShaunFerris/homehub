793854ee764f0dc294bbf9ff8cd8e4b8740b0e9d
Shaun Ferris <slb.ferris46@gmail.com>
2023-07-08 10:51:02 +0200

    Complete but untested budget save component

--- components/budget/BudgetSave.jsx
+++ components/budget/BudgetSave.jsx
@@ -8,17 +8,26 @@ const BudgetSave = ({ params }) => {
 
     const saveAndUpdate = async () => {
         try {
-            await fetch(`api/budget/${params.id}`, {
+            const response = await fetch(`api/budget/${params.id}`, {
                 method: "PATCH",
                 body: JSON.stringify({ ...budgetData })
             });
+            if (response.ok) {
+                console.log("Budget saved")
+                alert("Budget data has been successfully saved!")
+            }
         } catch (error) {
-
+            console.log("Failed to save budget: ", error);
         }
     };
 
     return (
-        <div>BudgetSave</div>
+        <button
+            className="black_btn"
+            onClick={saveAndUpdate}
+        >
+            Save Budget
+        </button>
     );
 };
 
