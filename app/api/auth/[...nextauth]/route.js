1c58d483f19513d48ea08e33c496454519dbac44
Shaun Ferris <slb.ferris46@gmail.com>
2023-07-20 13:39:08 +0200

    Remove export of authOptions that was breaking build

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@/utils/database";
 import User from "@/models/user";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
