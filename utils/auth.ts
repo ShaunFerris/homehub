c638d430ef569413bd14df36fa92e60e1833355c
Shaun Ferris <slb.ferris46@gmail.com>
2023-07-21 11:39:02 +0200

    Types for next-auth options and write a new typesafe authOptions object, exported from lib instead of directly on api route

---
+++ utils/auth.ts
@@ -0,0 +1,58 @@
+import { NextAuthOptions } from "next-auth";
+import { connectToDB } from "./database";
+import GoogleProvider from "next-auth/providers/google";
+import User from "@/models/user";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile) {
+        const sessionUser = await User.findOne({
+          email: profile.email
+        });
+
+        token.id = sessionUser._id.toString();
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        //check if the user already exists
+        //Missing await statement here caused the JWT error
+        const userExists = await User.findOne({
+          email: profile.email
+        });
+
+        //if not, return false to deny them access
+        if (!userExists) {
+          return false;
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking for user: ", error.message);
+        return false;
+      }
+    }
+  }
+};
