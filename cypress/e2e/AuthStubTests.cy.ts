d1ccd4fae2a5693fd514e56586fdf7365cd968af
Shaun Ferris <slb.ferris46@gmail.com>
2023-08-01 15:23:53 +0100

    Fixed the Auth stub tests to wait for session before checking for conditional renders

--- cypress/e2e/AuthStubTests.cy.ts
+++ cypress/e2e/AuthStubTests.cy.ts
@@ -3,16 +3,23 @@
  * logged in or logged out user state are working correctly
  */
 
-describe("Emulate login and logout on the home page", () => {
+describe("Loads login prompt when unauthenticated", () => {
   it("Visits the page and stubs logout", () => {
-    cy.visit("/");
     cy.logOut();
-    cy.get("[data-test='login-prompt']");
+    cy.visit("/");
+    cy.intercept("GET", "/api/auth/session").as("session");
+    cy.wait("@session").then(() => {
+      cy.get("[data-test='login-prompt']");
+    });
   });
 
-  it("visits the page and stubs login", () => {
-    cy.visit("/");
+  it("loads the menu when authenticated", () => {
+    cy.clearCookie("next-auth.session-token");
     cy.login(process.env.TEST_USER, process.env.TEST_PASS);
-    cy.get("[data-test='card-menu']");
+    cy.visit("/");
+    cy.intercept("GET", "/api/auth/session").as("session");
+    cy.wait("@session").then(() => {
+      cy.get("[data-test='card-menu']");
+    });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -30,5 +30,5 @@ Cypress.Commands.add("login", (username, password) => {
 
 Cypress.Commands.add("logOut", () => {
   //Currently stubbing a logged out state by just clearing the cookie
-  cy.intercept("api/auth/session", {});
+  cy.clearCookie("next-auth.session-token");
 });
