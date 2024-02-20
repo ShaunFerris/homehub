de83c0646d730ca7a26a91010530f205a5b5cf66
Shaun Ferris <slb.ferris46@gmail.com>
2024-02-20 11:52:17 +0100

    convert todo database model to typescript

---
+++ models/todo.ts
@@ -0,0 +1,30 @@
+import { Schema, Document, Model, model, models } from "mongoose";
+
+interface ITodo extends Document {
+  creator: Schema.Types.ObjectId;
+  name: string;
+  complete: boolean;
+}
+
+export interface ITodoModel extends Model<ITodo> {}
+
+const TodoSchema = new Schema<ITodo>({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  complete: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const Todo: ITodoModel =
+  models.Todo || model<ITodo, ITodoModel>("Todo", TodoSchema);
+
+export default Todo;
